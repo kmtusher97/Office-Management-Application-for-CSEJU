9bb41427bce4ad19b8e64f8fb2f166fe4a97c426
kmtusher97 <kmtusher97@gmail.com>
2020-01-14 05:01:36 +0600

    Topbar of the page

---
+++ syllabus-creator-client-app/src/components/topbar/Topbar.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import "./Topbar.css";
+
+class Topbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+        <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
+          CSEJU
+        </a>
+        <input
+          className="form-control form-control-dark w-100"
+          type="text"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap">
+            <a className="nav-link" href="#">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-search"
+              >
+                <circle cx="11" cy="11" r="8" />
+                <path d="M21 21l-4.35-4.35" />
+              </svg>
+            </a>
+          </li>
+        </ul>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap">
+            <a className="nav-link" href="#">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-bell"
+              >
+                <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
+                <path d="M13.73 21a2 2 0 0 1-3.46 0" />
+              </svg>
+            </a>
+          </li>
+        </ul>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap">
+            <a className="nav-link" href="#">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-mail"
+              >
+                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
+                <path d="M22 6l-10 7L2 6" />
+              </svg>
+            </a>
+          </li>
+        </ul>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap">
+            <a className="nav-link" href="#">
+              Sign out
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Topbar;
