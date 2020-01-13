5ed774433433686f65b6b92688bccecf9df3ede9
kmtusher97 <kmtusher97@gmail.com>
2020-01-14 05:00:15 +0600

    Add CSS to APP

--- syllabus-creator-client-app/src/App.js
+++ syllabus-creator-client-app/src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Topbar />
+
+      <div className="container-fluid">
+        <div className="row">
+          <Sidebar />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
