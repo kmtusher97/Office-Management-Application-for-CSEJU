feecbeafa28d509c695bbe10edcda2b44b958718
kmtusher97 <kmtusher97@gmail.com>
2020-01-14 08:57:35 +0600

    Syllabus Creator Initial

--- syllabus-creator-client-app/src/components/syllabus/Syllabus.js
+++ syllabus-creator-client-app/src/components/syllabus/Syllabus.js
@@ -1,11 +1,13 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 export default class Syllabus extends Component {
   render() {
     return (
-      <div>
-        <h2>Syllabus</h2>
-      </div>
+      <Fragment>
+        <div>
+          <h2>Syllabus</h2>
+        </div>
+      </Fragment>
     );
   }
 }
