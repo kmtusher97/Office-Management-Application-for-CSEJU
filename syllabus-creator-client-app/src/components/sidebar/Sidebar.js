663694f26d7d4f4b9fc3678e4cc6707187b54a36
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-01-17 22:33:09 +0600

    Seprating Sidebar menu files

--- syllabus-creator-client-app/src/components/sidebar/Sidebar.js
+++ syllabus-creator-client-app/src/components/sidebar/Sidebar.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./Sidebar.css";
 import { Link } from "react-router-dom";
 import Divider from "@material-ui/core/Divider";
+import MenuDashboard from "./MenuDashboard";
+import MenuProfile from "./MenuProfile";
 
 class Sidebar extends Component {
   render() {
@@ -9,45 +11,12 @@ class Sidebar extends Component {
       <nav className="col-md-2 d-none d-md-block bg-light sidebar">
         <div className="sidebar-sticky">
           <ul className="nav flex-column">
+            {this.props.children}
             <li className="nav-item">
-              <Link className="nav-link active" to={"/dashboard"}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="feather feather-home"
-                >
-                  <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                  <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                </svg>
-                Dashboard
-              </Link>
+              <MenuDashboard />
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={"/profile"}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="feather feather-user"
-                >
-                  <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
-                  <circle cx="12" cy="7" r="4" />
-                </svg>
-                Profile
-              </Link>
+              <MenuProfile />
             </li>
             <li className="nav-item">
               <Link className="nav-link" to={"/routine"}>
