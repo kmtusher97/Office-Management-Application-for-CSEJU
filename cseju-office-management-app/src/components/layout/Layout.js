3306fb9d73f73b9d12568878e5a0b612f0961395
kmtusher97 <kmtusher97@gmail.com>
2020-01-22 00:30:39 +0600

    Sidebar added and padding removed for menus

--- cseju-office-management-app/src/components/layout/Layout.js
+++ cseju-office-management-app/src/components/layout/Layout.js
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
+import { Row, Col, Container } from "react-bootstrap";
 import "./Layout.css";
 import Routing from "../../route/Routing";
 import Header from "./Header";
-import { Row, Col } from "react-bootstrap";
+import Sidebar from "./Sidebar";
 
 class Layout extends Component {
   render() {
     return (
       <div className="fluid">
         <Row className="header_container">
-          <Col md="12">
+          <Col md={12}>
             <Header />
           </Col>
         </Row>
-        <main className="main">
-          <Routing />
-        </main>
+        <Row>
+          <Col md={2}>
+            <Container className="sidebar_container">
+              <Sidebar />
+            </Container>
+          </Col>
+          <Col md={10}>
+            <main className="main">
+              <Routing />
+            </main>
+          </Col>
+        </Row>
       </div>
     );
   }
