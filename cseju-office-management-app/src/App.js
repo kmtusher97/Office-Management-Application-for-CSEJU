53c40d021bfa99bd0fbf71e35fc5a77a56c595ca
kmtusher97 <kmtusher97@gmail.com>
2020-01-21 13:56:16 +0600

    Routing to initial views

--- cseju-office-management-app/src/App.js
+++ cseju-office-management-app/src/App.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { Suspense, Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import SideNavbar from "./components/SideNavbar";
-import TopNavbar from "./components/TopNavbar";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <TopNavbar />
-        <SideNavbar />
-      </div>
-    </Router>
-  );
+import routes from "./Routes";
+
+class App extends Component {
+  loading = () => <div className="animated">Loading...</div>;
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <main className="main">
+            <Suspense fallback={this.loading()}>
+              <Switch>
+                {routes.map((route, idx) => (
+                  <Route
+                    key={idx}
+                    path={route.path}
+                    exact={route.exact}
+                    component={route.component}
+                  ></Route>
+                ))}
+                <Redirect from="/" to="/dashboard" />
+              </Switch>
+            </Suspense>
+          </main>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
