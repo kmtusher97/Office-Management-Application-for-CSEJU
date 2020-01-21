b9effde51c8d3f1437177c454c3ea95de7b6cb04
kmtusher97 <kmtusher97@gmail.com>
2020-01-21 14:13:18 +0600

    Move Components of App.js to Layout.js

--- cseju-office-management-app/src/App.js
+++ cseju-office-management-app/src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
-import Routing from "./route/Routing";
+import Layout from "./components/layout/Layout";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <main className="main">
-            <Routing />
-          </main>
+          <Layout />
         </div>
       </BrowserRouter>
     );
---
+++ cseju-office-management-app/src/components/layout/Layout.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Routing from "../../route/Routing";
+
+class Layout extends Component {
+  render() {
+    return (
+      <main className="main">
+        <Routing />
+      </main>
+    );
+  }
+}
+
+export default Layout;
