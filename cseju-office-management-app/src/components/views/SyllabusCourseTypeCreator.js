1786c3f9fa54d237841934e6fbbbdb95703c2e8d
kmtusher97 <kmtusher97@gmail.com>
2020-01-22 00:27:35 +0600

    Create initail views for the syllabus menus

---
+++ cseju-office-management-app/src/components/views/SyllabusCourseTypeCreator.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SyllabusCourseTypeCreator extends Component {
+  render() {
+    return (
+      <div>
+        <h1>SyllabusCourseTypeCreator</h1>
+      </div>
+    );
+  }
+}
+
+export default SyllabusCourseTypeCreator;
---
+++ cseju-office-management-app/src/components/views/SyllabusCreator.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SyllabusCreator extends Component {
+  render() {
+    return (
+      <div>
+        <h1>SyllabusCreator</h1>
+      </div>
+    );
+  }
+}
+
+export default SyllabusCreator;
