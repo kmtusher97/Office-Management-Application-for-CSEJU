d488ff5a70c07335c837794795baac8e4c68adb0
kmtusher97 <kmtusher97@gmail.com>
2020-01-21 13:54:21 +0600

    Initail View Files

---
+++ cseju-office-management-app/src/components/views/Syllabus.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Syllabus extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Syllabus</h1>
+      </div>
+    );
+  }
+}
+
+export default Syllabus;
