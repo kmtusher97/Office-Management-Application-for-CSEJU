09c81cdc62af8cd05df61021a8c281df6fd5d4e2
kmtusher97 <kmtusher97@gmail.com>
2020-01-24 05:11:33 +0600

    Change Styles for Sidebad

--- cseju-office-management-app/src/components/layout/Sidebar.js
+++ cseju-office-management-app/src/components/layout/Sidebar.js
@@ -3,6 +3,8 @@ import { Nav } from "react-bootstrap";
 import "./Sidebar.css";
 
 import SidebarRouting from "../../route/SidebarRouting";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 class Sidebar extends Component {
   constructor(props) {
@@ -14,7 +16,7 @@ class Sidebar extends Component {
   render() {
     return (
       <Nav
-        variant="pills"
+        variant="tabs"
         defaultActiveKey={
           "/" +
           this.state.pathnameComponents[1] +
@@ -23,7 +25,16 @@ class Sidebar extends Component {
         }
         className="sidebar flex-column shadow p-2 mb-1 bg-white rounded "
       >
-        <SidebarRouting />
+        <Row>
+          <Col md={12}>
+            <SidebarRouting />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <hr style={{ border: "1px solid gray", padding: "0px" }} />
+          </Col>
+        </Row>
       </Nav>
     );
   }
