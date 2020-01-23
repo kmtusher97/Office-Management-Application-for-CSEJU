082335bb4900ddbe124fda30bcc6ca1ff9934dbe
kmtusher97 <kmtusher97@gmail.com>
2020-01-24 04:46:34 +0600

    Place the page in center

--- cseju-office-management-app/src/components/layout/Layout.js
+++ cseju-office-management-app/src/components/layout/Layout.js
@@ -8,27 +8,30 @@ import Sidebar from "./Sidebar";
 class Layout extends Component {
   render() {
     return (
-      <div className="fluid">
-        <Row className="header_container">
-          <Col md={12}>
-            <Header />
-          </Col>
-        </Row>
-        <Row>
-          <Col md={2}>
-            <Container className="sidebar_container">
-              <Sidebar />
-            </Container>
-          </Col>
-          <Col md={10} style={{ paddingLeft: "0px", paddingRight: "30px" }}>
-            <main className="main ">
-              <div className="workspace fluid shadow p-2 mb-1 bg-white rounded">
-                <Routing />
-              </div>
-            </main>
-          </Col>
-        </Row>
-      </div>
+      <Row>
+        <Col md={1}></Col>
+        <Col md={10} className="fluid">
+          <Row className="header_container">
+            <Col md={12}>
+              <Header />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={2}>
+              <Container className="sidebar_container">
+                <Sidebar />
+              </Container>
+            </Col>
+            <Col md={10} style={{ paddingLeft: "0px", paddingRight: "30px" }}>
+              <main className="main ">
+                <div className="workspace fluid shadow p-2 mb-1 bg-white rounded">
+                  <Routing />
+                </div>
+              </main>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     );
   }
 }
