fd154aeecf73458f4220de8f1c798e4201b1e03d
kmtusher97 <kmtusher97@gmail.com>
2020-01-23 22:50:10 +0600

    JSON conversion issue fixed and scroll option added

--- cseju-office-management-app/src/components/syllabus/CourseInputFormCreator.js
+++ cseju-office-management-app/src/components/syllabus/CourseInputFormCreator.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Row, Col, Card, Container } from "react-bootstrap";
+import FormSection from "./courseInputFormComponents/FormSection";
 
 class CourseInputFormCreator extends Component {
   constructor(props) {
@@ -7,14 +8,14 @@ class CourseInputFormCreator extends Component {
     this.state = {
       syllabusName: this.props.match.params.syllabusName,
       courseTypeName: this.props.match.params.courseTypeName,
-      courseInputForm: null
+      courseInputForm: { courseInputFormSections: [] }
     };
   }
 
   componentDidMount() {
     /** Get Form Structure JSON */
     let url = `http://localhost:8081/syllabus/create_form/${this.state.syllabusName}/${this.state.courseTypeName}/get_form`;
-
+    console.log(url);
     fetch(url)
       .then(res => res.json())
       .then(result =>
@@ -28,7 +29,8 @@ class CourseInputFormCreator extends Component {
     return (
       <Container>
         <Row>
-          <Col md={12}>
+          <Col md={1}></Col>
+          <Col md={10}>
             <Card>
               <div style={{ paddingTop: "5px" }}>
                 <h6>Course Input Form for {this.state.courseTypeName}</h6>
@@ -37,8 +39,18 @@ class CourseInputFormCreator extends Component {
           </Col>
         </Row>
         <Row style={{ paddingTop: "10px" }}>
-          <Col md={12}>
-            <Card></Card>
+          <Col md={1}></Col>
+          <Col
+            md={10}
+            style={{ "overflow-y": "scroll", height: "580px", width: "auto" }}
+          >
+            {this.state.courseInputForm.courseInputFormSections.map(
+              (formSection, idx) => (
+                <div style={{ paddingTop: "10px" }}>
+                  <FormSection key={idx} formSectionData={formSection} />
+                </div>
+              )
+            )}
           </Col>
         </Row>
       </Container>
