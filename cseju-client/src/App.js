da022c93aa048370a90e0350e30d393978f95948
kmtusher97 <kmtusher97@gmail.com>
2020-01-31 00:37:31 +0600

    Add bootstarp to App.js and initial routes

--- cseju-client/src/App.js
+++ cseju-client/src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
 import "./App.css";
+/** Boot strap */
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Sidebar from "./components/layout/Sidebar";
+import Dashboard from "./components/Dashboard";
+import Routine from "./components/routine/Routine";
+import Syllabus from "./components/syllabus/Syllabus";
 
-class App extends Component() {
+class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App"></div>;
+        <div className="App">
+          <Header />
+          <Sidebar />
+
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/routine" component={Routine} />
+            <Route exact path="/syllabus" component={Syllabus} />
+          </Switch>
+        </div>
+        ;
       </Router>
     );
   }
--- cseju-client/src/index.js
+++ cseju-client/src/index.js
@@ -4,10 +4,6 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-/** Boot strap */
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-
 ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
