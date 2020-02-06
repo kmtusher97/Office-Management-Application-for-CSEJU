ce48cab38d2b3bd36ee954c48233aeff793057de
kmtusher97 <kmtusher97@gmail.com>
2020-02-06 15:36:38 +0600

    Update styles for AddFormSection Button

--- cseju-office-management-app/src/components/syllabus/courseInputFormComponents/SideMenusForForm.js
+++ cseju-office-management-app/src/components/syllabus/courseInputFormComponents/SideMenusForForm.js
@@ -1,32 +1,27 @@
 import React, { Component } from "react";
-import { Col } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 
 class SideMenusForForm extends Component {
   render() {
     return (
-      <Col md={1}>
-        <div
+      <div style={{ padding: "5px" }}>
+        <Button
+          size="sm"
+          variant="success"
+          onClick={this.props.addFormSection}
           style={{
-            height: "150px",
-            width: "30px"
+            fontFamily: "Arial, Helvetica, sans-serif",
+            float: "right"
           }}
         >
-          <div style={{ paddingTop: "2px" }}>
-            <button
-              className="btn"
-              style={{ border: "none", padding: 0, background: "none" }}
-            >
-              <span>
-                <i
-                  className="fa fa-plus"
-                  onClick={this.props.addFormSection}
-                  style={{ color: "green" }}
-                ></i>
-              </span>
-            </button>
-          </div>
-        </div>
-      </Col>
+          <span>
+            <FontAwesomeIcon icon={faPlusCircle} />
+          </span>
+          <b>{" Add Form Section"}</b>
+        </Button>
+      </div>
     );
   }
 }
